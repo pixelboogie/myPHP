c88324608637848ef3226f978fcbf1f6d22458d8
Shane Anderson <shane@pixelboogie.com>
2019-11-07 13:17:40 -0800

    good

--- DB_Interactions/config/config.php
+++ DB_Interactions/config/config.php
@@ -2,7 +2,7 @@
 
 // define('ROOT_URL', 'http://localhost/phpsandbox/website8/');
 define('DB_HOST', 'localhost');
-define('DB_USER', 'me   ');
+define('DB_USER', 'me');
 define('DB_PASS', 'password');
 define('DB_NAME', 'BlogDB');
 
---
+++ DB_Interactions/post.php
@@ -0,0 +1,40 @@
+<?php
+    require('config/config.php');
+
+    $query = 'SELECT * FROM  posts WHERE id = 1';
+
+    // Get result
+    $result = mysqli_query($conn, $query);
+
+    // Fetch data
+    $post = mysqli_fetch_assoc($result);
+    // var_dump($posts);
+
+
+    // Free result
+    mysqli_free_result($result);
+
+    //close connection
+    mysqli_close($conn);
+
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>PHP Blog</title>
+<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <H1><?php echo $post['title']; ?></h1>
+                            <small class="card-text">Created on <?php echo $post['created_at']; ?> by
+                            <?php echo $post['author']; ?></small>
+                            <p class="card-text"><?php echo $post['body']; ?></p>
+                            </div>
+                          
+                </div> 
+
+</body> 
+</html>
