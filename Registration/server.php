ea33c8b119cfa0664f2f81a1a40a347edcdc359b
Shane Anderson <shane@pixelboogie.com>
2019-11-06 17:35:58 -0800

    building server.php

--- Registration/server.php
+++ Registration/server.php
@@ -6,5 +6,27 @@ session_start();
 $username = "";
 $email = "";
 
+$error = array();
+
+
 //connect to db
-$db = mysqli_connect(
+$db = mysqli_connect('localhost', 'root', 'practiceDB') or die("could not connect to database");
+
+// Registered users
+$username = mysqli_real_escape_string($db, $_POST['username']);
+$email = mysqli_real_escape_string($db, $_POST['email']);
+$password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+$password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+
+//form validation
+if (empty($username)) {
+     array_push($errors, "username is required");
+    };
+if(empty($email)) {array_push($errors, "email is required")};
+if(empty($password_1)) {array_push($errors, "Password is required")};
+if($password_1 != $password_2) {array_push($erros, "Passwords do not match.")};
+
+?>
+
+
