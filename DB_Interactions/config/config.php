1bbb876451896d8ec89adff60935895dcece4564
Shane Anderson <shane@pixelboogie.com>
2019-11-07 13:21:29 -0800

    moved db and config to external files

--- DB_Interactions/config/config.php
+++ DB_Interactions/config/config.php
@@ -7,11 +7,4 @@ define('DB_PASS', 'password');
 define('DB_NAME', 'BlogDB');
 
 
-	// Create Connection
-	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-	// Check Connection
-	if(mysqli_connect_errno()){
-		// Connection Failed
-		echo 'Failed to connect to MySQL '. mysqli_connect_errno();
-	}
+?>
--- DB_Interactions/post.php
+++ DB_Interactions/post.php
@@ -1,5 +1,5 @@
 <?php
-    require('config/config.php');
+    require('config/db.php');
 
     $query = 'SELECT * FROM  posts WHERE id = 1';
 
