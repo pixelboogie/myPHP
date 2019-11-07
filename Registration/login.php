59b64d4fcbc265193e72fd854dbd8ca1c6221037
Shane Anderson <shane@pixelboogie.com>
2019-11-06 17:08:49 -0800

    firstr commit

---
+++ Registration/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE hmtl>
+<html>
+<head>
+    <title>Log in</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="header">
+        <h2>Log in</h2>
+    </div>
+    <form action="registration.php" method="post">
+        <div>
+            <label for="usernmae">Username: </label>
+            <input type="text" name="username">
+        </div>
+        <div>
+            <label for="usernmae">Password: </label>
+            <input type="text" name="username">
+        </div>
+
+        <button type="submit">Submit</button>
+        <p>Not a user?  <a href="register.php">Register Here</a></p>
+    </form>
+</div>
---
+++ Registration/register.php
@@ -0,0 +1,32 @@
+<!DOCTYPE hmtl>
+<html>
+<head>
+    <title>Registration </title>
+</head>
+<body>
+
+<div class="container">
+    <div class="header">
+        <h2>Registration</h2>
+    </div>
+    <form action="registration.php" method="post">
+        <div>
+            <label for="usernmae">Username: </label>
+            <input type="text" name="username">
+        </div>
+        <div>
+            <label for="usernmae">Email: </label>
+            <input type="text" name="username">
+        </div>
+        <div>
+            <label for="usernmae">Password: </label>
+            <input type="text" name="username">
+        </div>
+        <div>
+            <label for="usernmae">Confirm Password: </label>
+            <input type="text" name="username">
+        </div>
+        <button type="submit">Submit</button>
+        <p>Already a user?  <a href="login.php">Log in</a></p>
+    </form>
+</div>
